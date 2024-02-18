e5fdd679d2802b956fcd15ffcee14864a20fa38b
Paul Mojica <paulmojicatech@gmail.com>
2024-02-18 08:19:45 -0500

    add dependencies

--- pmtAuth/src/pmtAuth/Program.cs
+++ pmtAuth/src/pmtAuth/Program.cs
@@ -1,3 +1,5 @@
+using Swashbuckle.AspNetCore.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
 // package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
+// add swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+  options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+  {
+    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+    Name = "Authorization",
+    Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+  });
+  options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 var app = builder.Build();
 
